9cd46a979b0f9b50eb9ae0475f66423dc92d3394
shubhamverma845 <shubhamverma845@gmail.com>
2020-10-24 16:11:26 +0530

    Zuul Gatwaye Created

---
+++ mentorGateway/src/main/java/com/zuulForMentor/mentorGateway/MentorGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.zuulForMentor.mentorGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorGatewayApplication.class, args);
+	}
+
+}
---
+++ mentorGateway/src/test/java/com/zuulForMentor/mentorGateway/MentorGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuulForMentor.mentorGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
