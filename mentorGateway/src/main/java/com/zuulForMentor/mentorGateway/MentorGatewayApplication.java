14ef2b89945a57fc5ef630deecdbe76f001f90a1
shubhamverma845 <shubhamverma845@gmail.com>
2020-10-25 20:46:22 +0530

    Eureka Service Added

--- mentorGateway/src/main/java/com/zuulForMentor/mentorGateway/MentorGatewayApplication.java
+++ mentorGateway/src/main/java/com/zuulForMentor/mentorGateway/MentorGatewayApplication.java
@@ -2,8 +2,12 @@ package com.zuulForMentor.mentorGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MentorGatewayApplication {
 
 	public static void main(String[] args) {
